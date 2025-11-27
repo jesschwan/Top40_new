ff5b437e10f01ad4941d55deae5759c12b06639e
Jessica <jessica.schwanecke.azubi@icpmuenchen.de>
2025-11-27 11:43:08 +0100

    Angefang mit html und styles.css hinzugefügt und verlinkt im html Teil

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+?>
+
+<!-- HTML starts here ------------->
+ <!DOCTYPE html>
+ <html lang="de">
+    <head>
+        <meta charset="UTF-8">
+        <title>Top 40</title>
+        <link href="styles.css" rel="stylesheet">
